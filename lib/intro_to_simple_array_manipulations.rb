d784efcc15a5bc988f0b476890faa29d04598a64
Dawn Cronin <dawncronin7@gmail.com>
2019-10-10 20:04:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,6 +38,9 @@ def using_flatten(array)
   array.flatten 
 end
 
-def using_delete(array, int)
-  array.delete(int)
+def using_delete(array, string)
+  array.delete(string)
 end
+
+def using_delete_at(array, string)
+  array.delete_at(string)
